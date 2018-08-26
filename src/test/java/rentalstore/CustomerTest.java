1947afd9b5d5246ee4b515ee95b4165c006f8967
TANGTR2 <tang_hao1216@163.com>
2018-08-26 22:07:17 +0800

    [Tracy] pass test should_return_correct_HtmlStatement_given_customer_has_rent_one_regular_movie_for_1_day

--- src/test/java/rentalstore/CustomerTest.java
+++ src/test/java/rentalstore/CustomerTest.java
@@ -105,4 +105,18 @@ public class CustomerTest {
                 "<P>You owe<EM>0.0</EM><P>" + "\n" +
                 "On this rental you earned <EM>0</EM> frequent renter points<P>", statement);
     }
+
+    @Test
+    public void should_return_correct_HtmlStatement_given_customer_has_rent_one_regular_movie_for_1_day() {
+        Movie regularMovie = new Movie("Titanic", 0);
+        Rental oneDayRental = new Rental(regularMovie, 1);
+        customer.addRental(oneDayRental);
+
+        String statement = customer.HtmlStatement();
+
+        assertEquals("<H1>Rentals for <EM>Tracy</EM></H1><P>\n" +
+                "Titanic: 2.0<BR>\n" +
+                "<P>You owe<EM>2.0</EM><P>\n" +
+                "On this rental you earned <EM>1</EM> frequent renter points<P>", statement);
+    }
 }
