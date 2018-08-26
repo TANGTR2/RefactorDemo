ffc926a6ebaa44bf123c3d8af21a2a85b814a1e6
TANGTR2 <tang_hao1216@163.com>
2018-08-26 19:03:04 +0800

    [Tracy] rentalstore add test should_return_correct_statement_given_customer_has_no_rental

---
+++ src/test/java/rentalstore/CustomerTest.java
@@ -0,0 +1,16 @@
+package rentalstore;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class CustomerTest {
+
+    private Customer customer = new Customer("Tracy");
+
+    @Test
+    public void should_return_correct_statement_given_customer_has_no_rental() {
+        String statement = customer.statement();
+        assertEquals("Rental Record for Tracy\nAmount owed is 0.0\nYou earned 0 frequent renter points", statement);
+    }
+}
