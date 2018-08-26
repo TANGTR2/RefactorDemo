c2001e4b9330830917a12ef536ef302c60c70d65
TANGTR2 <tang_hao1216@163.com>
2018-08-26 19:06:44 +0800

    [Tracy] rentalstore add test should_return_correct_statement_given_customer_has_rent_one_regular_movie_for_3_day

--- src/test/java/rentalstore/CustomerTest.java
+++ src/test/java/rentalstore/CustomerTest.java
@@ -13,7 +13,7 @@ public class CustomerTest {
         String statement = customer.statement();
         assertEquals("Rental Record for Tracy\nAmount owed is 0.0\nYou earned 0 frequent renter points", statement);
     }
-    
+
     @Test
     public void should_return_correct_statement_given_customer_has_rent_one_regular_movie_for_1_day() {
         Movie regularMovie = new Movie("Titanic", 0);
@@ -25,6 +25,20 @@ public class CustomerTest {
         assertEquals("Rental Record for Tracy\n" +
                 "\t" + regularMovie.getTitle() + "\t2.0\n" +
                 "Amount owed is 2.0\n" +
+                "You earned 1 frequent renter points", statement);
+    }
+
+    @Test
+    public void should_return_correct_statement_given_customer_has_rent_one_regular_movie_for_3_day() {
+        Movie regularMovie = new Movie("Titanic", 0);
+        Rental threeDayRental = new Rental(regularMovie, 3);
+        customer.addRental(threeDayRental);
+
+        String statement = customer.statement();
+
+        assertEquals("Rental Record for Tracy\n" +
+                "\t" + regularMovie.getTitle() + "\t3.5\n" +
+                "Amount owed is 3.5\n" +
                 "You earned 1 frequent renter points", statement);
     }
 }
